a355521235cd48d9421894238dff0cf4c6124660
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-08-25 19:53:36 +0900

    Add Stateful widget in basic design

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,50 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp() );
 
 class MyApp extends StatelessWidget {
+  final title = "First App with Stateful";
+  final message = "statefulwidget";
 
   @override
   Widget build(BuildContext context){
 
-    return MaterialApp(
-        title: 'first step',
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('brawldra'),
-            ),
-            body: Text(
-                'Hello, Flutter World!!',
-                style: TextStyle(fontSize: 20)
-            ),
-
-        ),
+    return new MaterialApp(
+      title: 'first step',
+      home: new HomePage(
+        title: this.title,
+        message: this.message
+      ),
     );
+  }
+}
+
+class HomePage extends StatefulWidget{
+
+  final String title;
+  final String message;
+
+  HomePage({this.title, this.message}): super();
+
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+
+      body: Text(
+        widget.message,
+        style: TextStyle(fontSize: 20),
+      ),
+
+    );
+
   }
 }
 
