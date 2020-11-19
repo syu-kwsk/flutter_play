772dd5359b6ab586d646db4ed042df6b296fbfee
syu-kwsk <wataru1165kwsk@gmail.com>
2020-11-19 20:34:03 +0900

    Get quest data from firestore

--- flutter_practice_youtube/lib/main_model.dart
+++ flutter_practice_youtube/lib/main_model.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_practice_youtube/quest.dart';
 
 class MainModel extends ChangeNotifier {
   String myText = 'syu-kwsk';
+  List<Quest> questList = [];
 
   void changeMyText() {
     myText = 'proken216';
+    notifyListeners();
+  }
+
+  Future getQuestList() async {
+    final snapshot = await FirebaseFirestore.instance.collection('questList').get();
+    final docs = snapshot.docs;
+
+    final questList = docs.map((doc) => Quest(doc)).toList();
+    this.questList = questList;
     notifyListeners();
   }
 }
