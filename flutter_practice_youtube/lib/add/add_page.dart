0c04c2a04b7960ec8214dd5fcca924f407be11c1
syu-kwsk <wataru1165kwsk@gmail.com>
2020-11-20 17:48:53 +0900

    Add page to add quest

---
+++ flutter_practice_youtube/lib/add/add_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_practice_youtube/add/add_model.dart';
+
+class AddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  ChangeNotifierProvider<AddModel> (
+      create: (_) => AddModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('syu-kwsk'),
+        ),
+        body: Consumer<AddModel>(
+          builder: (context, model, child) {
+            return Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: '新クエスト',
+                        hintText: '例) 激辛の麻婆豆腐'
+                    ),
+                  ),
+                  RaisedButton(
+                      child: Text('追加'),
+                      onPressed: () {}),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
--- flutter_practice_youtube/lib/main.dart
+++ flutter_practice_youtube/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_practice_youtube/main_model.dart';
+import 'package:flutter_practice_youtube/add/add_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,47 +15,66 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: ChangeNotifierProvider<MainModel> (
-        create: (_) => MainModel()..getQuestListRealtime(),
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('syu-kwsk'),
-          ),
-          body: Consumer<MainModel>(
-            builder: (context, model, child) {
-              final questList = model.questList;
-              return Center(
-                child: Column(
-                  children: [
-                    Text(
-                      model.myText,
-                      style: TextStyle(
-                        fontSize: 30,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<MainModel> (
+      create: (_) => MainModel()..getQuestListRealtime(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('syu-kwsk'),
+        ),
+        body: Consumer<MainModel>(
+          builder: (context, model, child) {
+            final questList = model.questList;
+            return Center(
+              child: Column(
+                children: [
+                  Text(
+                    model.myText,
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  RaisedButton(
+                    child: Text('Button'),
+                    onPressed: () {
+                      // something
+                      model.changeMyText();
+                    },
+                  ),
+                  ListView(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children:
+                    questList.map(
+                          (quest) => ListTile(
+                        title: Text(quest.title),
+                        subtitle: Text(quest.createdAt.toString()),
                       ),
-                    ),
-                    RaisedButton(
-                      child: Text('Button'),
-                      onPressed: () {
-                        // something
-                        model.changeMyText();
-                      },
-                    ),
-                    ListView(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      children:
-                      questList.map(
-                            (quest) => ListTile(
-                          title: Text(quest.title),
-                          subtitle: Text(quest.createdAt.toString()),
-                        ),
-                      ).toList(),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
+                    ).toList(),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddPage(),
+                fullscreenDialog: true,
+              ),
+            );
+          },
+          child: Icon(Icons.add),
         ),
       ),
     );
