648dae7e22e865841a3e0a2b3d74d008bce79508
syu-kwsk <wataru1165kwsk@gmail.com>
2020-11-29 11:22:31 +0900

    Add quest function

--- flutter_practice_youtube/lib/add/add_model.dart
+++ flutter_practice_youtube/lib/add/add_model.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AddModel extends ChangeNotifier {
+  String questTitle = '';
+
+  Future addQuest() async {
+    final CollectionReference questListRef = FirebaseFirestore.instance.collection('questList');
+    await questListRef
+        .add({
+      'title': questTitle,
+      'createdAt': Timestamp.now()
+    })
+        .then((value) => print("Quest Added: $value"))
+        .catchError((error) => print("Failed to add quest: $error"));
+  }
 
 }
